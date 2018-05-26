110c1db2a2146df648c490869d4a50ce919b4d1d
Charlotte <wizleila@gmail.com>
2018-05-26 16:14:20 +0200

    upadte

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 run: compile
-		./Eval.ste
+		./eval.ste 1+3 4+5
 
 compile:
-		gcc -Wall main.c eval.c -o Eval.ste
+		gcc -Wall main.c liste.c -o eval.ste
 
 clean:
 		rm -f *.o
--- main.c
+++ main.c
@@ -1,19 +1,11 @@
-#include "eval.h"
+#include "liste.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char **argv)
 {
-  char* string;
-  liste_token* token;
 
-  token=initliste_token();
 
-  //scanf("%s",&string);
-
-  //printf("%s/n",&string);
-
-  freeliste_token(token);
 
   return 0;
 }
